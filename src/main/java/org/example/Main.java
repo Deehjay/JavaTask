aa277a9275a4f121f7cf2809dcd674edf1d1b51a
Dan James <41585360+Deehjay@users.noreply.github.com>
2026-08-14 21:10:15 +0100

    Add circle to pane and change background colour

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
-package org.example;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+public class Main extends Application
+{
+    @Override
+    public void start(Stage stage)
+    {
+        Pane root = new Pane();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        root.setStyle("-fx-background-color: grey;");
+
+        Circle circle = new Circle();
+
+        circle.setCenterX(400);
+        circle.setCenterY(300);
+        circle.setRadius(50);
+        circle.setFill(Color.WHITE);
+
+        root.getChildren().add(circle);
+
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("My JavaFX Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
